3ae89cbda4345b0cec20e9d7ffd05b762a0a1015
Kristjan Kask <kristjanvankask@gmail.com>
2021-11-13 14:37:51 +0200

    Added fields to Product model, Modified Category model. Registered the models to the admin Added media folder, installed Pillow, set URLs config to handle the media folder Registered the products app in settings Made migrations with the new models

--- products/models.py
+++ products/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=250)
-    image = models.ImageField()
-    price = models.FloatField()
-    category = models.Model(Category)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=255)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
