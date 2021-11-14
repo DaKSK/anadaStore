2aa369882ab537d48558cda1347eb98f005e06ca
Kristjan Kask <kristjanvankask@gmail.com>
2021-11-14 13:51:38 +0200

    Added ProductCreateView

--- storeAnada/views.py
+++ storeAnada/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from products.models import Product, Category
-
-
-class ProductCreateView(CreateView):
-    # template_name = "product_create.html"
-    # form_class = ProductCreateForm
-    # success_url = reverse_lazy("success")
-    pass
+from products.forms import ProductForm
 
 
 class ProductListView(ListView):
     template_name = "products/product_list.html"
     model = Product
+
+
+class ProductCreateView(CreateView):
+    template_name = "products/product_create.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('success')
